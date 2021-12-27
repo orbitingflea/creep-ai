f29b14d61c91b4bdcf2e32905b708fa4fd4a8c23
orbitingflea <orbitingflea@qq.com>
2021-12-27 14:58:32 +0800

    auto-upload

--- role.tower.js
+++ role.tower.js
@@ -4,6 +4,7 @@ var roleTower = {
         var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (target) {
             tower.attack(target);
+            return;
         }
         // repair damaged structure
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
