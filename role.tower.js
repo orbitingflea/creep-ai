7aa4bdfe31c5b5258d98b088b7300aa4daf8c172
orbitingflea <orbitingflea@qq.com>
2021-12-27 15:12:07 +0800

    auto-upload

--- role.tower.js
+++ role.tower.js
@@ -7,11 +7,12 @@ var roleTower = {
             return;
         }
         // repair damaged structure
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+            filter: (structure) => (structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL)
         });
         if (closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
+            return;
         }
     }
 }
