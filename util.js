babcfae416ad126ae8d285a88aa4b726e6a35383
orbitingflea <orbitingflea@qq.com>
2021-12-30 23:08:55 +0800

    auto-upload

--- util.js
+++ util.js
@@ -73,7 +73,7 @@ var util = {
 
     constant: utilConstant,
 
-    resourceTypeList: this.constant.resourceTypeList,
+    resourceTypeList: utilConstant.resourceTypeList,
 };
 
 module.exports = util;
