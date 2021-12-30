b9f9d2f7dc575f03cb7b0350c3b2f322da2ef73b
orbitingflea <orbitingflea@qq.com>
2021-12-30 10:44:49 +0800

    auto-upload

--- role.recycler.js
+++ role.recycler.js
@@ -5,24 +5,20 @@ module.exports = (args) => ({
     // collect dropped energy or tombstone's energy
 
     source: creep => {
-        if (creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) {
+        if (creep.store.getUsedCapacity() == creep.store.getCapacity()) {
             return true;
         }
 
         var room = Game.getObjectById(args.targetId).room;
-        const droppedList = room.find(FIND_DROPPED_RESOURCES, {
-            filter: (resource) => {
-                return resource.resourceType == RESOURCE_ENERGY;
-            }
-        });
+        const droppedList = room.find(FIND_DROPPED_RESOURCES);
         const tombList = room.find(FIND_TOMBSTONES, {
             filter: (tomb) => {
-                return tomb.store[RESOURCE_ENERGY];
+                return tomb.store.getUsedCapacity() > 0;
             }
         });
         const ruinList = room.find(FIND_RUINS, {
             filter: (ruin) => {
-                return ruin.store[RESOURCE_ENERGY];
+                return ruin.store.getUsedCapacity() > 0;
             }
         });
         if (droppedList.length == 0 && tombList.length == 0 && ruinList.length == 0) {
@@ -30,32 +26,51 @@ module.exports = (args) => ({
         }
         const source = creep.pos.findClosestByPath(droppedList.concat(tombList).concat(ruinList));
         if (!source) {
-            creep.say('Not Reachable');
+            creep.say('No Reachable');
             return false;
         }
 
-        if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ||
-            creep.pickup(source) == ERR_NOT_IN_RANGE) {
+        if (!creep.pos.inRangeTo(source, 1)) {
             creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             return false;
         }
-        if (creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) {
+
+        var result = creep.pickup(source);
+        if (result == ERR_INVALID_TARGET) {
+            // target need withdraw method
+            const types = util.resourceTypeList;
+            for (var i = 0; i < types.length; i++) {
+                var type = types[i];
+                if (source.store[type] > 0) {
+                    creep.withdraw(source, type);
+                }
+            }
+        }
+        if (creep.store.getUsedCapacity() == creep.store.getCapacity()) {
             return true;
         }
         return false;
     },
 
     target: creep => {
-        if (creep.store[RESOURCE_ENERGY] == 0) {
+        if (creep.store.getUsedCapacity() == 0) {
             return true;
         }
         const target = Game.getObjectById(args.targetId);
-        if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        if (!creep.pos.inRangeTo(target, 1)) {
             creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
             return false;
         }
-        if (creep.store[RESOURCE_ENERGY] == 0) {
+        const types = util.resourceTypeList;
+        for (var i = 0; i < types.length; i++) {
+            var type = types[i];
+            if (creep.store[type] > 0) {
+                creep.transfer(target, type);
+            }
+        }
+        if (creep.store.getUsedCapacity() == 0) {
             return true;
         }
+        return false;
     }
 });
