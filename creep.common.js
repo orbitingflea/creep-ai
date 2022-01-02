af68c17037900be6516ca339c466bac3dccba3e1
orbitingflea <orbitingflea@qq.com>
2022-01-02 17:54:20 +0800

    auto-upload

--- creep.common.js
+++ creep.common.js
@@ -6,7 +6,9 @@ var creepCommon = {
     // source、target 方法返回 true 就说明需要更换 working 状态
     // wait 方法在 source、target 均不能执行时运行，该状态不会被保存，wait 方法没有返回值
 
-    waitGotoPark: () => ((creep) => {
-        creep.moveTo(util.parkPosition(), { range: 1 });
-    }),
+    waitGotoPark: function() {
+        return (creep) => {
+            creep.moveTo(util.parkPosition(), { range: 1 });
+        }
+    },
 };
