8ed0cbb045daa1824ce38701ca1d3ff805cb7e49
orbitingflea <orbitingflea@qq.com>
2022-01-08 21:38:08 +0800

    auto-upload

--- memory.roomObject.js
+++ memory.roomObject.js
@@ -1,4 +1,19 @@
-RoomObject.prototype.memory = Memory.roomObjects[this.id];
+Object.defineProperty(RoomObject.prototype, 'memory', {
+    get: function() {
+        if (!Memory.roomObjects) {
+            //Memory.roomObjects = {};
+        }
+        return Memory.roomObjects[this.id];
+    },
+    set: function(value) {
+        if (!Memory.roomObjects) {
+            //Memory.roomObjects = {};
+        }
+        Memory.roomObjects[this.id] = value;
+    }
+});
+
+//RoomObject.prototype.memory = Memory.roomObjects[this.id];
 
 function CleanUp() {
     for (let id in Memory.roomObjects) {
