a3051482e5c2754747b9e435d3bde7562ebb3912
orbitingflea <orbitingflea@qq.com>
2022-01-08 21:29:46 +0800

    auto-upload

--- memory/roomObject.js
+++ memory/roomObject.js
@@ -1,9 +1,11 @@
 RoomObject.prototype.memory = Memory.roomObjects[this.id];
 
-export function CleanUp() {
+function CleanUp() {
     for (let id in Memory.roomObjects) {
         if (!Game.getObjectById(id)) {
             delete Memory.roomObjects[id];
         }
     }
 };
+
+module.exports.CleanUp = CleanUp;
