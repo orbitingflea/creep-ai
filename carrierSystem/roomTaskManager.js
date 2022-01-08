d9d0b1766018b7578b3f04dd3cb71f86d4a3e169
orbitingflea <orbitingflea@qq.com>
2022-01-08 20:08:55 +0800

    auto-upload

---
+++ carrierSystem/roomTaskManager.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+// define a prototype for the room task manager
+var RoomTaskManager = function(room) {
+    this.room = room;
+    this.taskList = [];
+};
+
+RoomTaskManager.prototype = {
+    // add a new task
+    
+};
---
+++ carrierSystem/task.js
@@ -0,0 +1,9 @@
+// @ts-check
+// This file defines the class 'task'
+// it describes a task that can be performed by a creep
+
+class Task {
+    constructor() {
+        
+    }
+}
